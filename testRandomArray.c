3b4b375525999264056d78302d247b30b0a8342e
Rodrigo Volpe Battistin <rv.battistin@usp.br>
2025-07-27 19:46:32 -0300

    Create script for testing random arrays
    The arrays will use the unsigned int type, because some of the sorting
    algorithms planned only work with positive integers, such as radix sort.
    For consistent testing, all algorithms should use the same array as input,
    therefore, the array's elements should satisfy the most strict condition
    
    Current implementation creates arrays of length 16 and contains numbers
    in the interval [0, 2^16 - 1].
    The seed for the pseudorandom number generator (rand from stdlib) can be
    set via argument in command line.

---
+++ testRandomArray.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void randomize_array(unsigned array[], short len, unsigned min, unsigned max);
+void print_array(unsigned array[], short len);
+
+int main(int argc, char *argv[]) {
+    unsigned array[16];
+
+    if (argc == 2) {
+        srand(atoi(argv[1]));
+    } else {
+        srand((unsigned) time(NULL));
+    }
+
+    randomize_array(array, 16, 0, 65535);
+
+    printf("Unsorted Array: ");
+    print_array(array, 16);
+    printf("\n");
+
+    return 0;
+}
+
+void randomize_array(unsigned array[], short len, unsigned min, unsigned max) {
+    double randFrac;
+    unsigned offset;
+
+    for (unsigned short i = 0; i < len; ++i) {
+        randFrac = (double) rand() / ((double) RAND_MAX + 1);
+        offset = (unsigned) (randFrac * (max + 1 - min));
+        array[i] = min + offset;
+    }
+}
+
+void print_array(unsigned array[], short len) {
+    (void) printf("[");
+    for (short i = 0; i < len - 1; ++i)
+        (void) printf("%u, ", array[i]);
+    (void) printf("%u]", array[len - 1]);
+}
