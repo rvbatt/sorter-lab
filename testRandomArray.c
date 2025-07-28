9511506e02cfd6ae006f9132ec193896fb1f0633
Rodrigo Volpe Battistin <rv.battistin@usp.br>
2025-07-28 08:56:11 -0300

    Allow arrays of arbitrary length
    - Implement error handling for the arguments passed via command line
    - Be careful with dynamic memory allocation

--- testRandomArray.c
+++ testRandomArray.c
@@ -1,32 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
-void randomize_array(unsigned array[], short len, unsigned min, unsigned max);
-void print_array(unsigned array[], short len);
+void randomize_array(unsigned *array, short len, unsigned min, unsigned max);
+void print_array(unsigned *array, short len);
 int compare_unsigned(const void *left, const void *right);
 
 int main(int argc, char *argv[]) {
-    unsigned array[16];
+    /*
+    Usage:
+    $ testRandomArray (<length>) (<seed>)
+        <length> (optional): length of the random array that will be tested. Default is 16;
+        <seed> (optional): the seed for the pseudorandom number generator.
+    */
+    short len;
+    unsigned *array;
+    char *debug;
 
-    if (argc == 2) {
-        srand(atoi(argv[1]));
-    } else {
-        srand((unsigned) time(NULL));
+    if (argc < 2) {
+        len = 16;
+    }
+    else {
+        len = (short) strtol(argv[1], &debug, 10);
+        if (strcmp(debug, "\0") != 0) {
+            printf("Invalid character in length: %s\n", debug);
+            return 1;
+        }
     }
 
-    randomize_array(array, 16, 0, 65535);
+    if (argc < 3) {
+        srand((unsigned) time(NULL));
+    }
+    else {
+        srand((unsigned) strtol(argv[2], &debug, 10));
+        if (strcmp(debug, "\0") != 0) {
+            printf("Invalid character in seed: %s\n", debug);
+            return 2;
+        }
+    }
+
+    array = (unsigned*) malloc(len * sizeof(unsigned));
+    randomize_array(array, len, 0, 65535);
 
     printf("Unsorted Array: ");
-    print_array(array, 16);
+    print_array(array, len);
     printf("\n");
 
-    qsort(array, 16, sizeof(unsigned), compare_unsigned);
+    qsort(array, len, sizeof(unsigned), compare_unsigned);
 
     printf("Sorted Array: ");
-    print_array(array, 16);
+    print_array(array, len);
     printf("\n");
 
+    free(array);
     return 0;
 }
 
@@ -34,7 +61,7 @@ int compare_unsigned(const void *left, const void *right) {
     return (*(unsigned *)left - *(unsigned *)right);
 }
 
-void randomize_array(unsigned array[], short len, unsigned min, unsigned max) {
+void randomize_array(unsigned *array, short len, unsigned min, unsigned max) {
     double randFrac;
     unsigned offset;
 
@@ -45,7 +72,7 @@ void randomize_array(unsigned array[], short len, unsigned min, unsigned max) {
     }
 }
 
-void print_array(unsigned array[], short len) {
+void print_array(unsigned *array, short len) {
     (void) printf("[");
     for (short i = 0; i < len - 1; ++i)
         (void) printf("%u, ", array[i]);
